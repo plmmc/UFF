7ca76a970c10ffcf0139954b3dd9364328ad4ac1
felipetp <fftepedino@gmail.com>
2018-11-18 14:00:02 -0200

    Add files via upload

---
+++ gaspipe.f90
@@ -0,0 +1,117 @@
+        program ferramentasmatematicas
+        implicit none
+        
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        !                                                    !
+        !        PEDRO LINS DE MOURA MARTINS DA COSTA        !
+        !                                                    !
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+        !!!!!!!!!!!
+        !VÁRIAVEIS!
+        !!!!!!!!!!!
+        
+        real :: p0, pL, L, zeta, f, D, ro, c, Q, S, a, b, n
+        real :: beta, alfa, dt, dx, 
+        integer :: i,j
+        real, dimension (n,n) :: A, B
+
+        a = dt/alfa*(dx)**2.0
+        b = a*beta*dx/2.0
+        zeta = (f/D)*((2.0*ro*c*Q/S)**2.0)
+
+        !!!!!!!!!!!!!!!!!!!!!!
+        !CONDIÇÃO DE CONTORNO!
+        !!!!!!!!!!!!!!!!!!!!!!
+
+        !pressão constante na saída do compressor
+        p(0) = 7.0 
+        
+        !assumimos que o último elemento discreto é regime permanente.
+        p(L) = SQRT((p0)**2.0-(zeta*L)) 
+
+
+        !!!!!!!!!!!!!!!!!!
+        !CONDIÇÃO INICIAL!
+        !!!!!!!!!!!!!!!!!!
+
+        do i=1,19
+        p(i) = 1
+        end do
+       
+        x(i) = 0 + (i)*dx
+        p(i) = 
+        end do
+
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        !DIAGONAL DE COEFICIENTES DA MATRIX A!
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        !preencher matrix com 0
+        do i = 1,n
+           do j = 1,n
+           A(i,j) = 0
+           endo do
+        end do
+
+        !diagonal principal
+        do i = 1,n
+          A(i,i) = (-2a - 2.0)
+        end do
+
+        !diagonal superior
+        do i = 2,n
+          A(i,i-1) = (a - b)
+        end do
+
+        !diagonal inferior
+        do i = 2, n
+           A(i-1,i) = (a + b)
+        end do
+        
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        !DIAGONAL DE COEFICIENTES DA MATRIX B!
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        !preencher matrix com 0
+        do i = 1,n
+           do j = 1,n
+           B(i,j) = 0
+           endo do
+        end do
+
+        !diagonal principal
+        do i = 1,n
+          B(i,i) = (2a - 2.0)
+        end do
+
+        !diagonal superior
+        do i = 2,n
+          B(i,i-1) = (-a + b)
+        end do
+
+        !diagonal inferior
+        do i = 2, n
+           B(i-1,i) = (-a - b)
+        end do
+
+        !contruindo os coeficiente de conhecidos u, matrix k
+
+        !computando o valor da equação de pressão para o tempo desejado
+
+        !algoritmo de Thomas
+
+        !contruindo matrix d
+
+        !construindo c'
+
+        !construindo d'
+
+        !calculando
+
+        !condição de contorno de Neuman
+
+        !saída de data
+
+        end program ferramentasmatematicas 
